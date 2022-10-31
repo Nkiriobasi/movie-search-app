ce1cbca4d8539b83461dc27a314ceb647d09ae7f
Nkiriobasi <obasinkiri@gmail.com>
2022-10-31 12:29:28 +0100

    created a header component with its minimal code setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = ({ text }) => {
+  return (
+    <header className='app__header'>
+        <h2>{text}</h2>
+    </header>
+  );
+
+}
+
+export default Header;
